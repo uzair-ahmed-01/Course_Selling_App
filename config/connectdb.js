e11b5672c3e5a2a704fb26fbdbea53cc39bca9b2
uzair-ahmed-01 <ansariuzair4151@gmail.com>
2023-11-06 21:40:29 +0530

    added constants.js and MongoDb Atlas

--- config/connectdb.js
+++ config/connectdb.js
@@ -1,14 +1,43 @@
 import mongoose from "mongoose";
+import DB_NAME from "../constants.js";
 
-const connectDB = async (DATABASE_URL) =>{
-    try{
-        const DB_OPTION ={
-            dbName :"courses"
-        }
-        await mongoose.connect(DATABASE_URL,DB_OPTION)
-        console.log("Connected Successfully...");
-    }catch(error){
-        console.log(error);
+// const connectDB = async (DATABASE_URL) =>{
+//     try{
+//         const DB_OPTION ={
+//             dbName :"courses"
+//         }
+//         await mongoose.connect(DATABASE_URL,DB_OPTION)
+//         console.log("Connected Successfully...");
+//     }catch(error){
+//         console.log(error);
+//     }
+// }
+
+// const connectDB = async () => {
+//     try {
+//         const connectionInstance = await mongoose.connect(`${process.env.DATABASE_URL}/${DB_NAME}`, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         });
+
+//         console.log(`MongoDB connected!! DB HOST: ${connectionInstance.connection.host}`);
+//     } catch (error) {
+//         console.error("MongoDB connection FAILED", error);
+//         process.exit(1);
+//     }
+// };
+
+const connectDB = async() => {
+    try {
+        await mongoose.connect(`${process.env.DATABASE_URL}/${DB_NAME}`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("MongoDB connected")
+
+    }catch(error) {
+        console.error("MongoDB connection FAILED", error);
+        process.exit(1);
     }
 }
 
---
+++ constants.js
@@ -0,0 +1,3 @@
+const DB_NAME = "courses";
+
+export default DB_NAME
